4086a9e90779bf6cdbbfbf6af28e56d13db0b88b
EC2 Default User <ec2-user@ip-172-31-4-41.ec2.internal>
2022-03-20 06:34:34 +0000

    コントローラ-作成

---
+++ app/controllers/customers/shipping_addresses_controller.rb
@@ -0,0 +1,35 @@
+class ShippingAddressesController < ApplicationController
+
+  def index
+    @shipping_address=ShippingAdress.new
+    @shipping_addresses=ShippingAdress.all
+  end
+
+  def create
+    @shipping_address=ShippingAdress.new(shipping_address_params)
+    @shipping_address.customer_id = current_customer.id
+    @shipping_address.save
+    redirect_to shipping_addresses_path
+  end
+
+  def edit
+    @shipping_addresses=ShippingAdress.find(params[:id])
+  end
+
+  def destroy
+     @shipping_address=ShippingAdress.find(params[:id])
+     @shipping_address.destroy
+     redirect_to shipping_addresses_path
+  end
+
+  def update
+    @shipping_address=ShippingAdress.find(params[:id])
+    @shipping_address.update(shipping_address_params)
+    redirect_to shipping_addresses_path
+  end
+
+  private
+  def shipping_address_params
+    params.require(:shipping_adress).permit(:shipping_post_code, :shipping_adress, :shipping_name)
+  end
+end
