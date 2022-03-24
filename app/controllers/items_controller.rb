a0cfccd48ef190b76f9aecdba8ff874b4758d1b0
kosuke0830 <taskforce0830@gmail.com>
2022-03-24 08:44:53 +0000

    ganbaruzo

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,16 @@
-class ItemsController < ApplicationController
-  def index
-  end
+class Customers::ItemsController < ApplicationController
 
-  def show
-  end
+ def index
+  @genres = Genre.all
+  @items = @search.result.page(params[:page]).per(8)
+  @items_all = Item.all
+ end
+
+ 
+ def show
+  @item = Item.find(params[:id])
+  @cart_item = Cart_Item.new
+  @genres = Genre.all
+ end
+
 end
