b243344e3c6f645588ef882ea48a29bd1b02d064
EC2 Default User <ec2-user@ip-172-31-22-21.ec2.internal>
2022-03-24 08:04:01 +0000

    [Finish]admin1-7

---
+++ app/controllers/admin/items_controller.rb
@@ -0,0 +1,44 @@
+class Admin::ItemsController < ApplicationController
+  before_action :authenticate_admin!
+
+  def index
+   @items = Item.all
+  end
+
+  def new
+   @item = Item.new
+  end
+
+  def create
+   @item = Item.new(item_params)
+   if @item.save!
+     redirect_to admin_item_path(@item.id)
+   else
+     render :new
+   end
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to admin_item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :image, :price, :status, :genre_id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip:[:registrations, :passwords], controllers: {
+  sessions:      'admin/sessions',
+}
+  namespace :admin do
+   resources :genres, only: [:index, :edit, :create, :update]
+   resources :items, only: [:index, :edit, :new, :show, :create, :update]
+
+  end
+
   devise_for :customers, controllers: {
   sessions:      'customers/sessions',
   passwords:     'customers/passwords',
   registrations: 'customers/registrations'
 }
-  devise_for :admins, controllers: {
-  sessions:      'admins/sessions',
-  passwords:     'admins/passwords',
-  registrations: 'admins/registrations'
-}
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
