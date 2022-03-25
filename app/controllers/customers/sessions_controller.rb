fcaa136a499320784185c81a5cd330174950e418
EC2 Default User <ec2-user@ip-172-31-4-41.ec2.internal>
2022-03-25 05:11:18 +0000

    退会機能修正

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -5,11 +5,13 @@ class Customers::SessionsController < Devise::SessionsController
 
  protected
   def reject_customer
-    @customer = Customer.find_by(name: params[:email][:encrypted_password])
+    @customer = Customer.find_by(email: params[:email][:encrypted_password])
     if @customer
-      if @customer.valid_password?(params[:email][:encrypted_password]) && (@customer.is_deleted == false)
+      if @customer.valid_password?(params[:email][:encrypted_password]) && (@customer.active_for_authentication? == false)
         flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
         redirect_to new_customer_registration_path
+      else
+        flash[:notice] = "項目を入力してください"
       end
     end
   end
