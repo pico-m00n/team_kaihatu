0654b814f87238a758f7c865a25fc038be8902b1
kosuke0830 <taskforce0830@gmail.com>
2022-03-26 06:56:43 +0000

    modify2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
-class Customers::ItemsController < ApplicationController
+class ItemsController < ApplicationController
 
  def index
   @genres = Genre.all
-  @items = @search.result.page(params[:page]).per(8)
+  @items = Item.all.page(params[:page]).per(8)
   @items_all = Item.all
  end
 
