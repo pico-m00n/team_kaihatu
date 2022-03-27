7719b6b99a26c80bc80715558000ee5442f6b1c4
EC2 Default User <ec2-user@ip-172-31-82-51.ec2.internal>
2022-03-27 11:33:00 +0000

    注文履歴、詳細画面

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,15 @@
 class Order < ApplicationRecord
  belongs_to :customer
  has_many :order_details
+	enum order_status: [:入金待ち, :入金確認, :製作中, :発送準備中, :発送済み]
+	enum payment: [:クレジットカード, :銀行振込]
 
- enum payment_method: { "クレジットカード": 0, "銀行振込": 1 }
-
-
-
-
-
+after_update do
+ if self.order_status == "入金確認"
+  self.order_details.each {|order_detail|
+  order_detail.update(item_status: "製作待ち")
+  }
+ end
+ end
 end
+
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,15 @@
 class OrderDetail < ApplicationRecord
  belongs_to :order
  belongs_to :item
+ enum item_status: [:着手不可, :製作待ち, :製作中, :製作完了]
+
+  after_update do
+    order_details = self.order.order_details
+    if order_details.any? {|order_detail| order_detail.item_status == "製作中"} == true
+      self.order.update(order_status: "製作中")
+    elsif order_details.all? {|order_detail| order_detail.item_status == "製作完了"} == true
+    self.order.update(order_status: "発送準備中")
+    end
+  end
 end
+
