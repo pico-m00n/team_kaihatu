5721f43d7b2412acbb9116b31133617057c0246b
EC2 Default User <ec2-user@ip-172-31-22-21.ec2.internal>
2022-03-27 06:49:48 +0000

    [Finish]header,about,session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-    customers_path
-  end
-
-  def after_sign_out_path_for(resource)
-     new_customer_session_path
-  end
+  
 
   protected
 
--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -3,6 +3,14 @@
 class Customers::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
   before_action :reject_customer, only: [:create]
+  
+  def after_sign_in_path_for(resource)
+    customers_path
+  end
+
+  def after_sign_out_path_for(resource)
+     new_customer_session_path
+  end
 
 
  protected
